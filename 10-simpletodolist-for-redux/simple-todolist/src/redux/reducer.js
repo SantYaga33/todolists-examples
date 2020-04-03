9e90b39a046a82c20b4b5fe96b1092959a706ba6
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-04-03 23:18:14 +0400

    clone project-> added REDUX +reducer

---
+++ 10-simpletodolist-for-redux/simple-todolist/src/redux/reducer.js
@@ -0,0 +1,57 @@
+export const ADD_TITLE = 'ADD_TITLE';
+export const ADD_TASK = 'ADD_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const CHANGE_STATUS = 'CHANGE_STATUS';
+
+export const onAddTitleAC = (newTitle) =>{
+	return { type:ADD_TITLE, newTitle }
+};
+export const onAddTaskeAC = (newTask) =>{
+	return { type:ADD_TASK, newTask }
+};
+export const onDeleteTaskAC = (taskId) =>{
+	return { type:DELETE_TASK, taskId }
+};
+export const onChangeStatusAC = (taskId, status) =>{
+	return { type:CHANGE_STATUS, taskId, status }
+};
+
+let initState = {
+	tasks: [
+		{ id: 0, title: 'CSS', checked: true },
+		{ id: 1, title:'JS', checked: true },
+		{ id: 2, title: 'REACT', checked: false },
+		{ id: 3, title: 'REDUX', checked: true }
+	],
+	title: '',
+	id: 5
+};
+
+
+export const reducer = (state = initState, action) => {
+
+	switch (action.type ) {
+		case ADD_TITLE:
+			return {
+				...state,
+				title: action.newTitle
+			};
+		case ADD_TASK:
+			return {
+				...state,
+				tasks: [...state.tasks, action.newTask],
+				id: state.id +1,
+				title: ''
+			}
+		case DELETE_TASK:
+			return {
+				...state,
+				tasks: state.tasks.map(task => task.id !== action.taskId),
+
+			};
+		default :
+			return state
+	}
+	return state
+
+};
--- 10-simpletodolist-for-redux/simple-todolist/src/ui/TodoList.js
+++ 10-simpletodolist-for-redux/simple-todolist/src/ui/TodoList.js
@@ -1,18 +1,75 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { onAddTaskeAC, onAddTitleAC, onChangeStatusAC, onDeleteTaskAC } from "../redux/reducer";
 
 const TodoList = (props) => {
+
+    const onAddTitle =(e) =>{
+        let newTitle = e.currentTarget.value;
+        props.onAddTitleStore(newTitle);
+
+    };
+    const onAddTask = () =>{
+        let newTask = 	{
+            id: props.id,
+            title: props.title,
+            checked: false
+        };
+        props.onAddTaskStore(newTask);
+    };
+
+    const onCheckedChanges = (e) =>{
+        // props.onChangeStatusStore (task.id, e.currentTarget.checked);
+        console.log( e.currentTarget.getAttribute('checked'));
+    };
+    const onDeleteTask = (e) =>{
+        // props.onDeleteTaskStore(task.id);
+        console.log( e.currentTarget.getAttribute('id'));
+    };
+
+    let newTasks = props.tasks.map(task =>  <li key={task.id} id={task.id}><input type="checkbox" checked={task.checked}
+                                            onChange={onCheckedChanges}/>
+                                            {task.title}<button id={task.id} onClick={onDeleteTask}>X</button></li>);
     return (
         <div>
             <h1>Todo List</h1>
-            <div><input /><button>+</button></div>
+            <div><input onChange={onAddTitle} value={props.title}/>
+            <button onClick={onAddTask}>+</button></div>
             <ul>
-                <li><input type="checkbox" /> CSS <button>X</button></li>
-                <li><input type="checkbox" /> JS <button>X</button></li>
-                <li><input type="checkbox" /> HTML <button>X</button></li>
-                <li><input type="checkbox" /> REACT JS <button>X</button></li>
+                {newTasks}
             </ul>
         </div>
     )
-}
+};
 
-export default TodoList;
+const mapStatetoProps = (state) =>{
+  return {
+      tasks: state.tasks,
+      id: state.id,
+      title: state.title
+  }
+};
+const mapdispatchtoProps = (dispatch) =>{
+  return {
+      onAddTitleStore: (newTitle) =>{
+          const action = onAddTitleAC(newTitle);
+          dispatch(action);
+      },
+      onAddTaskStore: (newTask) =>{
+          const action = onAddTaskeAC(newTask);
+          dispatch(action);
+      },
+      onDeleteTaskStore: (taskId) =>{
+          const action = onDeleteTaskAC(taskId);
+          dispatch(action);
+      },
+      onChangeStatusStore: (taskId, status) =>{
+          const action = onChangeStatusAC(taskId,status);
+          dispatch(action);
+      }
+    }
+};
+
+const connectTodolist = connect(mapStatetoProps, mapdispatchtoProps )(TodoList);
+
+export default connectTodolist;
